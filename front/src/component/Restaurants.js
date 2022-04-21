a611c2be8513fb4b4fccd67763963c933656972d
lebelebe <jhw9627@gmail.com>
2022-04-21 15:51:24 +0900

    랜덤뽑기세팅

--- front/src/component/Restaurants.js
+++ front/src/component/Restaurants.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Restaurants(){
+function Restaurants(props){
 
+    // Math.random으로 난수 생성후 length만큼 곱하고 1더해서 1부터 시작 + 소수점날림 
+    const randomNumber = Math.floor(Math.random() * 10 +1)
+
+    return(
+        <div>
+
+        </div>
+    )
 }
 
 export default Restaurants;
