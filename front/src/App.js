3725476484e06f2a2ac6e1724e45769d4741d90b
lebelebe <jhw9627@gmail.com>
2022-04-22 12:11:21 +0900

    post도전

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Insertform from './component/Insertform';
 
 function App() {
   return (
     <div className="App">
-
+      <Insertform></Insertform>
     </div>
   );
 }
--- front/src/component/RestarurantInsert.js
+++ front/src/component/RestarurantInsert.js
@@ -1,8 +1,13 @@
-import React from "react"
-import {ErrorMessage, useformik} from 'formik'
+import React, {useState} from "react"
+import {useFormik} from 'formik';
+import * as Yup from "yup";
+import axios from 'axios';
 
 function RestaurantInsert(props){
-    const selectList = ["한식", "양식", "중식", "일식", "패스트푸드", "기타"]
+    const selectList = ["한식", "양식", "중식", "일식", "기타"]
+    const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+    const [message, setMessage ] = useState('');
+    
 
     const validate = values => {
         const errors = {};
@@ -20,11 +25,42 @@ function RestaurantInsert(props){
         initialValues: {
           bc_name: '',
           bc_value: '',
+          bc_map: '',
         },
+        validationSchema: Yup.object({
+            bc_name: Yup.string().required("이름은 필수입니다."),
+            bc_map: Yup.string().required("주소는 필수입니다."),
+            bc_info: Yup.string().max(100, "100자를 초과할 수 없습니다.")
+          }),
         onSubmit: async (values) => {
             await sleep(500);
-            alert(JSON.stringify(values, null, 2));
-        },
+            // alert(JSON.stringify(values, null, 2));
+            try{
+                axios.post('/api?type='+props.dbinfo.type,
+                    {
+                        headers: {
+                            "Content-Type": `application/json`
+                        },
+                        body: values
+                    })
+                    .then( lunch => {
+                        if(lunch.data == 'success'){
+                            setMessage('노드에 접속 성공')
+                        }
+                        else{
+                            setMessage('쿼리 혹은 xml 접속문제 존재')
+                        }
+                    })
+                    .catch(
+                        (err) => {
+                            setMessage('답 못가져옴 파일 조사' +err)
+                        }
+                    )
+            }
+            catch(err){
+                setMessage('서버연결안됨' + err)
+            }
+        }
       });
     return(
         <div>
@@ -40,8 +76,9 @@ function RestaurantInsert(props){
                         name="bc_name"
                         type="text"
                         onChange={formik.handleChange}
-                        value={formik.values.bc_name} />
-                {formik.touched.bc_name && formik.errors.bc_name ? (<div>{formik.errors.bc_name}</div>): null}
+                        value={formik.values.bc_name} 
+                        {...formik.getFieldProps("bc_name")}/>
+                {formik.touched.bc_name && formik.errors.bc_name ? (<div className="input-feedback">{formik.errors.bc_name}</div>): null}
                 <label htmlFor="bc_category">카테고리</label>
                     <select
                         id="bc_category"
@@ -60,15 +97,26 @@ function RestaurantInsert(props){
                     name="bc_map"
                     type="text"
                     onChange={formik.handleChange}
-                    value={formik.values.bc_map} />
+                    value={formik.values.bc_map} 
+                    {...formik.getFieldProps("bc_map")}/>
+                {formik.touched.bc_map && formik.errors.bc_map ? (<div className="input-feedback">{formik.errors.bc_map}</div>): null}
                 <label htmlFor="bc_info">가게정보</label>
-                    <input
+                    <textarea
                         id="bc_info"
                         name="bc_info"
                         type="textarea"
+                        rows="5"
+                        cols="33"
                         onChange={formik.handleChange}
-                        value={formik.values.bc_info} />
+                        value={formik.values.bc_info}
+                        {...formik.getFieldProps("bc_info")}>
+                    </textarea>
+                    {formik.touched.bc_info && formik.errors.bc_info ? (<div className="input-feedback">{formik.errors.bc_info}</div>): null}
+                <button type="submit">
+                    등록
+                </button>
             </form>
+            <p>{message}</p>
         </div>
     )
 }
